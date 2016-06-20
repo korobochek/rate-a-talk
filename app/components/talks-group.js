07bc3ce92a063bd0f16a6a66e8ec907463d956ef
Olga Goloshchapova <olga.goloshchapova@rea-group.com>
2016-06-21 09:24:47 +1000

    not sure why styling does not work, but later

--- app/components/talks-group.js
+++ app/components/talks-group.js
@@ -1,6 +1,8 @@
+import $ from 'jquery'
 import React, { Component } from 'react'
 
 import Talk from 'components/talk'
+import 'stylesheets/talks-group.scss'
 
 export default class TalksGroup extends Component {
   static propTypes = {
@@ -8,13 +10,28 @@ export default class TalksGroup extends Component {
     grouping: React.PropTypes.string
   }
 
+  toggleMondaySessions() {
+    if ($('#toggle').hasClass('fa-angle-down')) {
+      $('#toggle').removeClass('fa-angle-down')
+      $('#toggle').addClass('fa-angle-up')
+      $('#yesterday-talks').css('display', 'inline-flex')
+    } else {
+      $('#toggle').removeClass('fa-angle-up')
+      $('#toggle').addClass('fa-angle-down')
+      $('#yesterday-talks').css('display', 'none')
+    }
+  }
+
   render() {
+    const grouping = this.props.grouping
+    const yesterdayTalksId = grouping === 'Monday 20 June 2016' ? 'yesterday-talks' : ''
     return (
       <div>
-        <div className="flexible-box date-title">
-          <h2>{this.props.grouping}</h2>
+        <div className="flexible-box date-title" onClick={event => { this.toggleMondaySessions() }}>
+          <h2>{grouping}</h2>
+          { yesterdayTalksId != '' ? <i id="toggle" className="fa fa-angle-down"></i> : null}
         </div>
-        <div className="flexible-box">
+        <div className="flexible-box" id={yesterdayTalksId}>
           { this.props.talks ?
           this.props.talks.map(talk =>
             (<div className="flexible-component" key={talk.id}><Talk talk={talk}></Talk></div>)) : null
